6acbe2580c1bfe13881a47652450393e9fa30ecf
Kellie Dinh <dinhkellie@yahoo.com>
2017-09-14 17:02:53 -0400

    shorten bio and working on hide/show timeline

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$('#past').on('click', function()) {
-              
-              }
+function past() {
+    var x = document.getElementById('pastBody');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+    
+    var y = document.getElementById('pastTitle');
+    var z = document.getElementById('pastTitle2');
+    if (y.style.display = "none") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        z.style.display = "block";
+        y.style.display = "none";
+    }
+    
+}
