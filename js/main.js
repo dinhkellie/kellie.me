b0473e4bdf5075c1e6a3ff7b5ec759391b36de6f
Kellie Dinh <dinhkellie@yahoo.com>
2017-09-17 03:40:15 -0400

    working dropdown tabs

--- js/main.js
+++ js/main.js
@@ -5,10 +5,50 @@ function past() {
     } else {
         x.style.display = 'none';
     }
-    
+
     var y = document.getElementById('pastTitle');
     var z = document.getElementById('pastTitle2');
-    if (y.style.display = "none") {
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        z.style.display = "block";
+        y.style.display = "none";
+    }
+    
+}
+
+function present() {
+    var x = document.getElementById('presentBody');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+
+    var y = document.getElementById('presentTitle');
+    var z = document.getElementById('presentTitle2');
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        z.style.display = "block";
+        y.style.display = "none";
+    }
+    
+}
+
+function future() {
+    var x = document.getElementById('futureBody');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+
+    var y = document.getElementById('futureTitle');
+    var z = document.getElementById('futureTitle2');
+    if (y.style.display === "none") {
         y.style.display = "block";
         z.style.display = "none";
     } else {
